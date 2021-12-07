324705b510adcd7d1d16ac36b9d8c8a7c27e426c
yakmax <yakmax@gmail.com>
2021-12-07 05:20:02 +0500

    Initial commit

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CoffeeApp.Startup))]
+namespace CoffeeApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
